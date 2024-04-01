0c1f95e9b6e4108b42774f0d94029d233ae3314d
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-04-01 13:32:34 +0200

    create auth data source

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,38 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signUpWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+}
