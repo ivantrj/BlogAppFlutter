b4723d75cb797b05324da061b30a8c9ee3185e88
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-04-11 08:46:43 +0200

    app wide user, move entity user to core

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
@@ -10,6 +11,7 @@ void main() async {
   await initDependencies();
   runApp(MultiBlocProvider(
     providers: [
+      BlocProvider(create: (_) => serviceLocator<AppUserCubit>()),
       BlocProvider(
         create: (_) => serviceLocator<AuthBloc>(),
       ),
@@ -38,7 +40,21 @@ class _MainAppState extends State<MainApp> {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightThemeMode,
       darkTheme: AppTheme.darkThemeMode,
-      home: const LoginPage(),
+      home: BlocSelector<AppUserCubit, AppUserState, bool>(
+        selector: (state) {
+          return state is AppUserLoggedIn;
+        },
+        builder: (context, isLoggedIn) {
+          if (isLoggedIn) {
+            return const Scaffold(
+              body: Center(
+                child: Text("Logged In"),
+              ),
+            );
+          }
+          return const LoginPage();
+        },
+      ),
     );
   }
 }
